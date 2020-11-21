ed6371302fcd46da6caf435d699991bd1778451a
Eva <egutierrez@educem.com>
2020-11-21 20:11:17 +0100

    Nou Toolbar customitzat i amb menu

--- app/src/main/java/com/example/menustoolbars/MainActivity.java
+++ app/src/main/java/com/example/menustoolbars/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.menustoolbars;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setTitle(null);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: mostrar About
+                Toast.makeText(MainActivity.this, "Has clicat al menu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_toolbar, menu);
+        menu.findItem(R.id.action_settings).setVisible(true);
         return true;
     }
 
